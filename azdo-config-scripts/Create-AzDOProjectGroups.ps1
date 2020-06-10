ee968973b117cf2e96f4f2f3b9e7926396a3ee62
Bradley Rees <bradley.rees@azure.lloydsbank.com>
2020-06-10 18:05:45 +0000

    Updated Create-AzDOProjectGroups.ps1 to add option for configuring live tenant

--- azdo-config-scripts/Create-AzDOProjectGroups.ps1
+++ azdo-config-scripts/Create-AzDOProjectGroups.ps1
@@ -1,5 +1,5 @@
 ###
-#
+# 
 # This script is used to create organisation & project level groups for new project creation and performs most of 
 # the standard assignments in line with the IAM documentation in confluence here:
 # https://confluence.devops.lloydsbanking.com/pages/viewpage.action?pageId=367101137
@@ -7,40 +7,66 @@
 ### Params to pass in
 param(
     [string] 
-    $org = "lbg-cloudfirst",
+    $org = "",
     [string]
     $project = "Build Release Spike",
     [string]
-    $prjAbrv = "BRS"
+    $prjAbrv = "BRS",
+    # switch to determine if changes are you be made in prod or non-prod tenant
+    [bool]
+    $prod = $false
 )
 ###
 
-# Deines the list of Organisation level Groups to be created
-$orgGroups = [ordered]@{
-    "ADO_CNEB_PRJ_CyberEng" = "PROJ - Cyber security engineers on the project";
-    "ADO_CNEB_PRJ_DevLeads" = "PROJ - Dev leads are generally the Engineering Leads within a Lab and share some of the SRE tasks together with an elevated level sign off";
-    "ADO_CNEB_PRJ_Devs" = "PROJ - Developers on the project";
-    "ADO_CNEB_PRJ_Guest" = "PROJ - Guest user with reader permissions to the Project";
-    "ADO_CNEB_PRJ_LabSRE" = "PROJ - Guest user with reader permissions to the Project";
-    "ADO_CNEB_PRJ_ProjectMgmt" = "PROJ - Group for the Project Management function, purpose is to act as the release gate where process requires it";
-    "ADO_CNEB_PRJ_SnrDevs" = "PROJ - Senior Devs are the next level down from Dev Leads and are generally responsible for a more senior level of approval in the PR process";
-    "ADO_CNEB_PRJ_SrvAcc" = "PROJ - Project level service account"
+if ([string]::IsNullOrEmpty($org)) {
+    if ($prod) { $org = "lbgcloudlive" } else { $org = "lbg-cloudfirst" }
 }
 
-#Defines the list of Project level groups to be created
-$prjGroups = [ordered]@{
-    "SignOff_Cyber" = "Sign off groups for changes requiring Cyber Security approval";
-    "SignOff_IAM" = "Sign off group for IAM related changes";
-    "SignOff_NetSec" = "Sign off group for Network Security related changes";
-    "SignOff_PolicyOwner" = "Sign off group for Policy Owners";
-    "SignOff_ReleaseMgmt" = "Sign off group for where a release needs approval";
-    "SignOff_SeniorEng" = "Sign off group for senior engineers";
-    "Signoff_Strategy" = "Sign off group Strategy & Direction Team";
-    "SignOff_SRE" = "Sign off group for SRE";
-    "SignOff_WAGF" = "Sign off group for WAFG changes";
-    "Service Accounts" = "Team for non-human service accounts that perform automated tasks"
-}
+# initialise our hash tables
+$orgGroups, $prjGroups = @{}
 
+if ($prod) {
+    # Deines the list of Organisation level Groups to be created
+    $orgGroups = [ordered]@{
+        "ADO_CNEL_PRJ_Guest" = "PROJ - Guest user with reader permissions to the Project";
+        "ADO_CNEL_PRJ_LabSRE" = "PROJ - Guest user with reader permissions to the Project";
+        "ADO_CNEL_PRJ_ProjectMgmt" = "PROJ - Group for the Project Management function, purpose is to act as the release gate where process requires it";
+        "ADO_CNEL_PRJ_SrvAcc" = "PROJ - Project level service account"
+    }
+
+    #Defines the list of Project level groups to be created
+    $prjGroups = [ordered]@{
+        "SignOff_ReleaseMgmt" = "Sign off group for where a release needs approval";
+        "Service Accounts" = "Team for non-human service accounts that perform automated tasks"
+    }
+}
+else {
+    # Deines the list of Organisation level Groups to be created
+    $orgGroups = [ordered]@{
+        "ADO_CNEB_PRJ_CyberEng" = "PROJ - Cyber security engineers on the project";
+        "ADO_CNEB_PRJ_DevLeads" = "PROJ - Dev leads are generally the Engineering Leads within a Lab and share some of the SRE tasks together with an elevated level sign off";
+        "ADO_CNEB_PRJ_Devs" = "PROJ - Developers on the project";
+        "ADO_CNEB_PRJ_Guest" = "PROJ - Guest user with reader permissions to the Project";
+        "ADO_CNEB_PRJ_LabSRE" = "PROJ - Guest user with reader permissions to the Project";
+        "ADO_CNEB_PRJ_ProjectMgmt" = "PROJ - Group for the Project Management function, purpose is to act as the release gate where process requires it";
+        "ADO_CNEB_PRJ_SnrDevs" = "PROJ - Senior Devs are the next level down from Dev Leads and are generally responsible for a more senior level of approval in the PR process";
+        "ADO_CNEB_PRJ_SrvAcc" = "PROJ - Project level service account"
+    }
+
+    #Defines the list of Project level groups to be created
+    $prjGroups = [ordered]@{
+        "SignOff_Cyber" = "Sign off groups for changes requiring Cyber Security approval";
+        "SignOff_IAM" = "Sign off group for IAM related changes";
+        "SignOff_NetSec" = "Sign off group for Network Security related changes";
+        "SignOff_PolicyOwner" = "Sign off group for Policy Owners";
+        "SignOff_ReleaseMgmt" = "Sign off group for where a release needs approval";
+        "SignOff_SeniorEng" = "Sign off group for senior engineers";
+        "Signoff_Strategy" = "Sign off group Strategy & Direction Team";
+        "SignOff_SRE" = "Sign off group for SRE";
+        "SignOff_WAGF" = "Sign off group for WAFG changes";
+        "Service Accounts" = "Team for non-human service accounts that perform automated tasks"
+    }
+}
 $grpDescriptors = @{}
 
 # Tests the output from the az command for errors
@@ -115,7 +141,7 @@ Function Add-Groups() {
 }
 
 # Set AzDO defaults for CLI
-az devops configure --defaults organization=https://dev.azure.com/lbg-cloudfirst project=$project
+az devops configure --defaults organization=https://dev.azure.com/$($org) project=$project
 
 # Cycle through and create all the org level groups
 $orgGroups.GetEnumerator() | ForEach-Object {
@@ -135,42 +161,58 @@ $prjGroups.GetEnumerator() | ForEach-Object {
 
 $grpHash = Create-GroupsHash
 
-# now add the org (& project) level groups as member od the right project groups
-Add-Groups -groups $grpHash -parent "$project/SignOff_Cyber"                    -children @("$org/ADO_CNEB_$($prjAbrv)_CyberEng")
-Add-Groups -groups $grpHash -parent "$project/SignOff_NetSec"                   -children @("$org/ADO_CNEB_GLO_NetSec")
-Add-Groups -groups $grpHash -parent "$project/SignOff_IAM"                      -children @("$org/ADO_CNEB_GLO_IAM")
-Add-Groups -groups $grpHash -parent "$project/SignOff_PolicyOwner"              -children @("$org/ADO_CNEB_GLO_PolicyOwner")
-Add-Groups -groups $grpHash -parent "$project/SignOff_ReleaseMgmt"              -children @("$org/ADO_CNEB_$($prjAbrv)_ProjectMgmt")
-Add-Groups -groups $grpHash -parent "$project/SignOff_SeniorEng"                -children @("$org/ADO_CNEB_$($prjAbrv)_DevLeads",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_SnrDevs")
-Add-Groups -groups $grpHash -parent "$project/SignOff_SRE"                      -children @("$org/ADO_CNEB_$($prjAbrv)_LabSRE")
-Add-Groups -groups $grpHash -parent "$project/SignOff_Strategy"                 -children @("$org/ADO_CNEB_GLO_Strategy")
-Add-Groups -groups $grpHash -parent "$project/SignOff_WAGF"                     -children @("$org/ADO_CNEB_GLO_WAGF")
+if ($prod) {
+    Add-Groups -groups $grpHash -parent "$project/SignOff_ReleaseMgmt"              -children @("$org/ADO_CNEL_$($prjAbrv)_ProjectMgmt")
+    Add-Groups -groups $grpHash -parent "$project/Build Administrators"             -children @("$project/Service Accounts")
+    Add-Groups -groups $grpHash -parent "$project/Contributors"                     -children @("$org/ADO_CNEL_$($prjAbrv)_LabSRE")
+    Add-Groups -groups $grpHash -parent "$project/Endpoint Administrators"          -children @("$project/Service Accounts")
+    Add-Groups -groups $grpHash -parent "$project/Project Administrators"           -children @("$org/ADO_CNEL_GLO_PlatformSRE")
+    Add-Groups -groups $grpHash -parent "$project/Readers"                          -children @("$project/Contributors",
+                                                                                                "$project/Service Accounts",
+                                                                                                "$org/ADO_CNEL_$($prjAbrv)_Guest",
+                                                                                                "$project/SignOff_ReleaseMgmt")
+    Add-Groups -groups $grpHash -parent "$project/Service Accounts"                 -children @("$org/ADO_CNEL_$($prjAbrv)_SrvAcc",
+                                                                                                "$org/ADO_CNEL_GLO_SrvAcc")
 
-Add-Groups -groups $grpHash -parent "$project/Build Administrators"             -children @("$project/Service Accounts",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_DevLeads",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_LabSRE")
-Add-Groups -groups $grpHash -parent "$project/Contributors"                     -children @("$org/ADO_CNEB_$($prjAbrv)_CyberEng",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_DevLeads",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_Devs",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_LabSRE",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_ProjectMgmt",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_SnrDevs")
-Add-Groups -groups $grpHash -parent "$project/Deployment Group Administrators"  -children @("$org/ADO_CNEB_$($prjAbrv)_LabSRE")
-Add-Groups -groups $grpHash -parent "$project/Endpoint Administrators"          -children @("$project/Service Accounts",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_LabSRE")
-Add-Groups -groups $grpHash -parent "$project/Project Administrators"           -children @("$org/ADO_CNEB_GLO_PlatformSRE")
-Add-Groups -groups $grpHash -parent "$project/Readers"                          -children @("$project/Contributors",
-                                                                                            "$project/Service Accounts",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_Guest",
-                                                                                            "$project/SignOff_Cyber",
-                                                                                            "$project/SignOff_NetSec",
-                                                                                            "$project/SignOff_PolicyOwner",
-                                                                                            "$project/SignOff_ReleaseMgmt",
-                                                                                            "$project/SignOff_SeniorEng",
-                                                                                            "$project/SignOff_SRE",
-                                                                                            "$project/SignOff_WAGF")
-Add-Groups -groups $grpHash -parent "$project/Release Administrators"           -children @("$org/ADO_CNEB_$($prjAbrv)_DevLeads",
-                                                                                            "$org/ADO_CNEB_$($prjAbrv)_LabSRE")
-Add-Groups -groups $grpHash -parent "$project/Service Accounts"                 -children @("$org/ADO_CNEB_$($prjAbrv)_SrvAcc",
-                                                                                            "$org/ADO_CNEB_GLO_SrvAcc")
+}
+else {
+    # now add the org (& project) level groups as member od the right project groups
+    Add-Groups -groups $grpHash -parent "$project/SignOff_Cyber"                    -children @("$org/ADO_CNEB_$($prjAbrv)_CyberEng")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_NetSec"                   -children @("$org/ADO_CNEB_GLO_NetSec")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_IAM"                      -children @("$org/ADO_CNEB_GLO_IAM")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_PolicyOwner"              -children @("$org/ADO_CNEB_GLO_PolicyOwner")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_ReleaseMgmt"              -children @("$org/ADO_CNEB_$($prjAbrv)_ProjectMgmt")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_SeniorEng"                -children @("$org/ADO_CNEB_$($prjAbrv)_DevLeads",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_SnrDevs")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_SRE"                      -children @("$org/ADO_CNEB_$($prjAbrv)_LabSRE")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_Strategy"                 -children @("$org/ADO_CNEB_GLO_Strategy")
+    Add-Groups -groups $grpHash -parent "$project/SignOff_WAGF"                     -children @("$org/ADO_CNEB_GLO_WAGF")
+
+    Add-Groups -groups $grpHash -parent "$project/Build Administrators"             -children @("$project/Service Accounts",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_DevLeads",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_LabSRE")
+    Add-Groups -groups $grpHash -parent "$project/Contributors"                     -children @("$org/ADO_CNEB_$($prjAbrv)_CyberEng",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_DevLeads",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_Devs",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_LabSRE",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_ProjectMgmt",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_SnrDevs")
+    Add-Groups -groups $grpHash -parent "$project/Deployment Group Administrators"  -children @("$org/ADO_CNEB_$($prjAbrv)_LabSRE")
+    Add-Groups -groups $grpHash -parent "$project/Endpoint Administrators"          -children @("$project/Service Accounts",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_LabSRE")
+    Add-Groups -groups $grpHash -parent "$project/Project Administrators"           -children @("$org/ADO_CNEB_GLO_PlatformSRE")
+    Add-Groups -groups $grpHash -parent "$project/Readers"                          -children @("$project/Contributors",
+                                                                                                "$project/Service Accounts",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_Guest",
+                                                                                                "$project/SignOff_Cyber",
+                                                                                                "$project/SignOff_NetSec",
+                                                                                                "$project/SignOff_PolicyOwner",
+                                                                                                "$project/SignOff_ReleaseMgmt",
+                                                                                                "$project/SignOff_SeniorEng",
+                                                                                                "$project/SignOff_SRE",
+                                                                                                "$project/SignOff_WAGF")
+    Add-Groups -groups $grpHash -parent "$project/Release Administrators"           -children @("$org/ADO_CNEB_$($prjAbrv)_DevLeads",
+                                                                                                "$org/ADO_CNEB_$($prjAbrv)_LabSRE")
+    Add-Groups -groups $grpHash -parent "$project/Service Accounts"                 -children @("$org/ADO_CNEB_$($prjAbrv)_SrvAcc",
+                                                                                                "$org/ADO_CNEB_GLO_SrvAcc")
+}
